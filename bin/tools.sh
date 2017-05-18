5d91b28ec0bedf95d5deac3d304f340a2ce8e598
walkeryg <ygcom@aliun.com>
2017-05-19 07:51:33 +0800

    add tmux jd tldr in tools.sh & project.rc exmaple

--- bin/tools.sh
+++ bin/tools.sh
@@ -1,2 +1,8 @@
 sudo apt-get install silversearcher-ag
 sudo apt-get install rxvt-unicode
+sudo apt-get install tmux
+sudo apt-get install jq
+
+location=/usr/local/bin/tldr
+sudo wget -qO $location https://raw.githubusercontent.com/pepa65/tldr-bash-client/master/tldr
+sudo chmod +x $location
