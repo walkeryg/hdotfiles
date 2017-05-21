65dca07702d9fa6cbb496d63314874999158f539
walkeryg <ygcom@aliun.com>
2017-05-21 22:34:02 +0800

    add roxterm & terminology ppa

--- bin/tools.sh
+++ bin/tools.sh
@@ -6,3 +6,12 @@ sudo apt-get install jq
 location=/usr/local/bin/tldr
 sudo wget -qO $location https://raw.githubusercontent.com/pepa65/tldr-bash-client/master/tldr
 sudo chmod +x $location
+
+sudo add-apt-repository ppa:niko2040/e19
+sudo apt-get update
+sudo apt-get install enlightenment
+sudo apt-get install terminology
+
+sudo add-apt-repository ppa:h-realh/roxterm
+sudo apt-get update
+sudo apt-get install roxterm
