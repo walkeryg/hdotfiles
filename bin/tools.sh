7040254ac87510386eba5135539e9556ae24283b
walker_yang <walker_yang@realsil.com.cn>
2018-11-08 08:03:29 +0800

    add useful tools

--- bin/tools.sh
+++ bin/tools.sh
@@ -12,6 +12,19 @@ curl -sLf https://spacevim.org/install.sh | bash
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 
+# change brew mirror source
+cd "$(brew --repo)"
+git remote set-url origin https://mirrors.ustc.edu.cn/brew.git
+
+cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
+
+# reset brew source
+# cd "$(brew --repo)"
+# git remote set-url origin https://github.com/Homebrew/brew.git
+
+# cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+# git remote set-url origin https://github.com/Homebrew/homebrew-core.git
 echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
 
 brew install the_silver_searcher
@@ -21,6 +34,14 @@ brew install jq
 brew install tree
 brew install tldr
 brew install neovim
+brew install bat
+brew install prettyping
+brew install fzf
+brew install htop
+brew install diff-so-fancy
+brew install fd
+brew install ncdu
+brew install noti
 
 # location=/usr/local/bin/tldr
 # sudo wget -qO $location https://raw.githubusercontent.com/pepa65/tldr-bash-client/master/tldr
