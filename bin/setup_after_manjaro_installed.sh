cf4827c4e62337c109096b90597327fc6e5c2724
walkeryg <ygcom@aliyun.com>
2019-08-10 12:33:59 +0800

    update manjaro relate sh

--- bin/setup_after_manjaro_installed.sh
+++ bin/setup_after_manjaro_installed.sh
@@ -7,9 +7,12 @@ sudo pacman-mirrors -i -c China -m rank
 sudo pacman -Syy 
 
 # add archlinuxcn source
-sudo echo "[archlinuxcn]" >> /etc/pacman.conf
-sudo echo "SigLevel = Optional TrustedOnly" >> /etc/pacman.conf
-sudo echo "Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch" >> /etc/pacman.conf
+sudo cp /etc/pacman.conf /etc/pacman.conf.bak
+cp /etc/pacman.conf /tmp/pacman.conf
+echo "[archlinuxcn]" >> /tmp/pacman.conf
+echo "SigLevel = Optional TrustedOnly" >> /tmp/pacman.conf
+echo "Server = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/$arch" >> /tmp/pacman.conf
+sudo cp /tmp/pacman.conf /etc/pacman.conf
 
 # install archlinuxcn-keyring
 sudo pacman -Syy
@@ -18,17 +21,8 @@ sudo pacman -Syyu
 
 sudo pacman -S google-chrome
 
-chsh -s /bin/zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
-git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
 sudo pacman -S autojump
 
-# plugins=( git zsh-syntax-highlighting zsh-autosuggestions autojump zsh-nvm )
-# ZSH_THEME="agnoster"
-# export NVM_LAZY_LOAD=true
 
 curl -sLf https://spacevim.org/install.sh | bash
 
@@ -40,6 +34,12 @@ echo "export GTK_IM_MODULE=fcitx" >> ~/.xprofile
 echo "export QT_IM_MODULE=fcitx" >> ~/.xprofile
 echo "export XMODIFIERS="@im=fcitx"" >> ~/.xprofile
 sudo pacman -S fcitx-qt4
+sudo pacman -S wqy-microhei
+sudo pacman -S bat
+sudo pacman -S fzf
+sudo pacman -S prettyping
+sudo pacman -S ncdu
+sudo pacman -S tldr
 # sudo pacman -S electronic-webchat
 
 sudo pacman -S yay base-devel
---
+++ bin/setup_myzsh.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+chsh -s /bin/zsh
+if [ -d ~/.oh-my-zsh ]; then
+  echo "oh my zsh already exist";
+else
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+  # git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+  git clone https://github.com/trapd00r/zsh-syntax-highlighting-filetypes.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting-filetypes
+  cp ~/.oh-my-zsh/templates/zshrc.zsh-template zshrc
+  dircolors LS_COLORS >> zshrc
+
+  cat custom_cmd_alias >> zshrc
+  my zshrc ~/.zshrc
+  # plugins=( git zsh-syntax-highlighting zsh-autosuggestions autojump zsh-nvm )
+  # ZSH_THEME="agnoster"
+  # export NVM_LAZY_LOAD=true
+fi
+
