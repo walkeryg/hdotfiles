c6fb3a06c8777b0b34d2acb729f4592f71a89091
walker_yang <walker_yang@realsil.com.cn>
2022-07-08 17:49:22 +0800

    add some tools

--- bin/setup_qemu.sh
+++ bin/setup_qemu.sh
@@ -1,6 +1,7 @@
 sudo apt install qemu-system-arm qemu-efi
 sudo apt install qemu-user
 sudo apt install gcc-arm-linux-gnueabihf
+sudo apt install gdb-multiarch
 
 cd test/qemu
 arm-linux-gnueabihf-gcc -static test.c -o test 
